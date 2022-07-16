2953c4fa6d5c12ba4aa09bda1122f7e9debc3622
Steven Valladares <stevenvalladaresgiron@gmail.com>
2022-07-16 16:43:40 -0400

    The script completes to validate the entered data

--- assets/js/index.js
+++ assets/js/index.js
@@ -29,6 +29,34 @@ function showCards(cardContainer) {
     cardContainer.innerHTML = html
 }
 
+function search() {
+    // get elements HTML
+    const numberOfRooms = parseInt(document.getElementById('input1').value)
+    const minSquareMeters = parseInt(document.getElementById('input2').value)
+    const maxSquareMeters = parseInt(document.getElementById('input3').value)
+    // display data entered in the console
+    console.log('number of rooms: '.concat(numberOfRooms).concat(' type: '.concat(typeof(numberOfRooms))))
+    console.log('min meters: '.concat(minSquareMeters).concat(' type: '.concat(typeof(minSquareMeters))))
+    console.log('max meters: '.concat(maxSquareMeters).concat(' type: '.concat(typeof(maxSquareMeters))))
+    // validate inputs
+    if (isNaN(numberOfRooms) || isNaN(minSquareMeters) || isNaN(maxSquareMeters)) {
+        alert('- Faltan campos por llenar'.concat('\n'.concat('- Asegúrese de introducir valores numéricos')))
+        return location.reload()
+    }
+    if (numberOfRooms > 5) {
+        alert('El número máximo de habitaciones disponibles por propiedad es de 5')
+        return location.reload()
+    }
+    if (minSquareMeters >= maxSquareMeters) {
+        alert('El alcance mínimo de metros cuadrados no puede ser mayor que el alcance máximo')
+        return location.reload()
+    }
+    
+}
+
 //variables 
 const cardContainer = document.querySelector('.properties') 
 showCards(cardContainer)
+
+const btn = document.getElementById('btn')
+btn.addEventListener('click', search)
